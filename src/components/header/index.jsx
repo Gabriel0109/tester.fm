e9e206da7f29bb27204c41b6dcecb3f1943f1a3e
gabriel0109 <gabrielglfg@outlook.com>
2022-05-31 15:30:24 -0300

    f

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,18 +1,88 @@
 import { IntroWrapper } from './styles.js';
-
+import {useEffect, useState} from 'react';
+import axios from 'axios'
 
 export function Header() {
     const CLIENT_ID = "7db72dde18e949d280daba96bf9d69e9"
-    const REDIRECT_URI = "https://localhost:3000/callback"
+    const REDIRECT_URI = "http://localhost:3000/"
     const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
     const RESPONSE_TYPE = "token"
+    const SCOPES = [
+        "ser-read-recently-played",
+        "user-read-email",
+        " user-top-read",
+        "user-read-currently-playing",
+        "user-read-private"
+    ]
+
+
+    const [token, setToken] = useState("")
+    // const [searchKey, setSearchKey] = useState("")
+    // const [artists, setArtists] = useState([])
+        
+    useEffect(() => {
+        const hash = window.location.hash
+        let token = window.localStorage.getItem("token")
+
+        if (!token && hash) {
+            token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
+
+            window.location.hash = ""
+            window.localStorage.setItem("token", token)
+        }
+
+        setToken(token)
+
+    }, [])
+
+    const logout = () => {
+        setToken("")
+        window.localStorage.removeItem("token")
+    }
+
+    // const searchArtists = async (e) => {
+    //     e.preventDefault()
+    //     const {data} = await axios.get("https://api.spotify.com/v1/search", {
+    //         headers: {
+    //             Authorization: `Bearer ${token}`
+    //         },
+    //         params: {
+    //             q: searchKey,
+    //             type: "artist"
+    //         }
+    //     })
+        
+    //     setArtists(data.artists.items)
+    //     console.log(token)
+
+    // }
+    // const renderArtists = () => {
+    //     return artists.map(artist => (
+    //         <div key={artist.id}>
+    //             {artist.images.length ? <img width={"100%"} src={artist.images[0].url} alt=""/> : <div>No Image</div>}
+    //             {artist.name}
+    //         </div>
+    //     ))
+    // }
 
     return (
-
+        
         <>
             <IntroWrapper>
-                <h1>Lierbag</h1>
-                <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login to Spotify</a>
+                {!token ?
+                    <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login
+                        to Spotify</a>
+                    : <button onClick={logout}>Logout</button>}
+
+                {token ?
+                  <h1>Hello, {}</h1>
+               
+
+                    : <h2>Please login</h2>
+                }
+
+                {/* {renderArtists()} */}
+
             </IntroWrapper>
         </>
     );
