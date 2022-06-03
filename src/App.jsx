0dcbe190014e41c8f6e613ba58e70ed739a30ea8
Gabriel Guimarães <82231197+Gabriel0109@users.noreply.github.com>
2022-06-03 13:26:10 -0300

    teste1001

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { Header } from './components/header/index.jsx'
 import { SessionProvider } from './hooks/useSession.tsx'
-import { HoverBoxes } from "./components/hoverboxes/HoverBoxes.jsx";
 import './globals.css';
 function App() {
 
   return (
     <SessionProvider>
       <Header />
-      <HoverBoxes />
     </SessionProvider>
   )
 }
