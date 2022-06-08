cde2c21714b4c90ce59ed1ad517bfbe9d36d68d9
Gabriel Guimarães <82231197+Gabriel0109@users.noreply.github.com>
2022-06-08 17:33:40 -0300

    multiple axios requests

--- src/components/navboxes/index.jsx
+++ src/components/navboxes/index.jsx
@@ -3,15 +3,21 @@ import { Box1 } from './box1/index.jsx'
 import { Box2 } from './box2/index.jsx'
 import { Box3 } from './box3/index.jsx'
 import { Box4 } from './box4/index.jsx'
+import { FaSpotify } from 'react-icons/fa'
+import { useSession } from "../../hooks/useSession";
+
 
 export function Navboxes() {
+    const { refresh } = useSession()
 
     return (<>
         <BoxesWrapper>
             <Box1>
-                <h1>1</h1>
-                <div className="text-flow">
-                    teste teste teste
+                <div className="spotify-logo">
+                    <button href="" onClick={refresh} >
+                        <FaSpotify />
+                        <p>Open in Spotify</p>
+                    </button>
                 </div>
             </Box1>
             <Box2 />
--- src/hooks/useSession.tsx
+++ src/hooks/useSession.tsx
@@ -10,6 +10,7 @@ interface contextInterface {
     RESPONSE_TYPE: string;
     SCOPES: string[];
     logout: () => void 
+    refresh: () => void 
 
 }
 const loginContext = createContext<contextInterface>({} as contextInterface)
@@ -18,10 +19,10 @@ const loginContext = createContext<contextInterface>({} as contextInterface)
 export function SessionProvider({ children }){
   
     const CLIENT_ID = "7db72dde18e949d280daba96bf9d69e9"
-    const REDIRECT_URI = "https://main--classy-druid-749ea2.netlify.app/"
+    const REDIRECT_URI = "http://localhost:3000"
     const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
     const RESPONSE_TYPE = "token"
-    const SCOPES = ["user-read-currently-playing"]
+    const SCOPES = ["user-read-currently-playing", "user-read-recently-played"]
     const [token, setToken] = useState("")
     const [user, setUser] = useState("")
     useEffect(() => {
@@ -32,29 +33,57 @@ export function SessionProvider({ children }){
             token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
             window.location.hash = ""
             localStorage.setItem("token", token)
-            try{
-                const url = "https://api.spotify.com/v1/me";
-                axios.get(url, {
+        }
+        try {
+            // const url = "https://api.spotify.com/v1/me";
+            // axios.get(url, {
+            //     headers: {
+            //         Authorization: `Bearer ${token}`
+            //     },
+            // }).then(response => {
+            //     setUser(response.data.id)
+            // })
+                // const url2 = "https://api.spotify.com/v1/me/player/currently-playing";
+            // axios.get(url2, {
+            //     headers: {
+            //         Authorization: `Bearer ${token}`
+            //     },
+            // }).then(response => {
+            //     console.log(response.data.item)
+
+            // })
+            let one = "https://api.spotify.com/v1/me"
+            let two = "https://api.spotify.com/v1/me/player/currently-playing"
+            let three = "https://api.spotify.com/v1/me/player/recently-played?limit=5"
+            const requestOne = axios.get(one, {
                     headers: {
                         Authorization: `Bearer ${token}`
                     },
-                }).then(response => {
-                    setUser(response.data.id)
-                })
-                const url2 = "https://api.spotify.com/v1/me/player/currently-playing";
-                axios.get(url2, {
-                    headers: {
-                        Authorization: `Bearer ${token}`
-                    },
-                }).then(response => {
-                    console.log(response.data.item)
-
-                })
-            } catch{
-                return console.log('oi')
-            }
-         
-
+                } );
+            const requestTwo = axios.get(two, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                },
+            });
+            const requestThree = axios.get(three, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                },
+            });
+            axios
+                .all([requestOne, requestTwo, requestThree])
+                .then(
+                    axios.spread((...responses) => {
+                        const responseOne = responses[0];
+                        const responseTwo = responses[1];
+                        const responseThree = responses[2];
+                        setUser(responseOne.data.id)
+                      console.log(responseTwo.data)
+                      console.log(responseThree)
+                    })
+                )
+        } catch {
+            return console.log('oi')
         }
         setToken(token)
     }, [])
@@ -64,10 +93,21 @@ export function SessionProvider({ children }){
         window.localStorage.removeItem("token")
         document.location.reload();
     }
+    const refresh = () => {
+         const url2 = "https://api.spotify.com/v1/me/player/currently-playing";
+                axios.get(url2, {
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    },
+                }).then(response => {
+                    console.log(response.data)
+
+                })
+    }
 
 
 
-    return ( <loginContext.Provider value={{CLIENT_ID, REDIRECT_URI, AUTH_ENDPOINT, RESPONSE_TYPE, token, user, SCOPES, logout }}>
+    return ( <loginContext.Provider value={{CLIENT_ID, REDIRECT_URI, AUTH_ENDPOINT, RESPONSE_TYPE, token, user, SCOPES, logout, refresh }}>
             {children}
              </loginContext.Provider>
         
